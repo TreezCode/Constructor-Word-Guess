ce5624017b4fa62e4e2ee38e2aca2444de4a67de
TreezCode <TreezCode@users.noreply.github.com>
2019-05-26 19:32:28 -0600

    display word on win/loss

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Import files and packages
 const Word = require("./word.js");
 const inquirer = require("inquirer");
@@ -6,58 +5,58 @@ const colors = require("colors");
 
 // GLobal Variables
 const wordBank = [
-  "Acai", 
+  "Acai",
   "Apple",
   "Apricot",
-  "Avocado", 
-  "Banana", 
-  "Blackberry", 
-  "Blueberry", 
-  "Boysenberry", 
-  "Cantaloupe", 
-  "Cherry", 
-  "Chili pepper", 
+  "Avocado",
+  "Banana",
+  "Blackberry",
+  "Blueberry",
+  "Boysenberry",
+  "Cantaloupe",
+  "Cherry",
+  "Chili pepper",
   "Coconut",
-  "Corn kernel", 
-  "Crab apples", 
-  "Cranberry", 
+  "Corn kernel",
+  "Crab apples",
+  "Cranberry",
   "Cucumber",
-  "Currant", 
-  "Date", 
-  "Dragonfruit", 
+  "Currant",
+  "Date",
+  "Dragonfruit",
   "Eggplant",
-  "Elderberry", 
-  "Fig", 
-  "Goji berry", 
-  "Grape", 
-  "Grapefruit", 
-  "Guava", 
-  "Huckleberry", 
-  "Jackfruit", 
-  "Juniper berry", 
-  "Kiwi", 
-  "Kumquat", 
-  "Lemon", 
-  "Lime", 
-  "Mango", 
-  "Melon", 
-  "Nectarine", 
-  "Orange", 
-  "Papaya", 
-  "Passionfruit", 
-  "Peach", 
-  "Pear", 
-  "Pineapple", 
-  "Plantain", 
-  "Plum", 
+  "Elderberry",
+  "Fig",
+  "Goji berry",
+  "Grape",
+  "Grapefruit",
+  "Guava",
+  "Huckleberry",
+  "Jackfruit",
+  "Juniper berry",
+  "Kiwi",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Mango",
+  "Melon",
+  "Nectarine",
+  "Orange",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Pineapple",
+  "Plantain",
+  "Plum",
   "Pomegranate",
-  "Pumpkin", 
-  "Raspberry", 
-  "Star apple", 
-  "Star fruit", 
-  "Strawberry", 
-  "Tomato", 
-  "Zucchini", 
+  "Pumpkin",
+  "Raspberry",
+  "Star apple",
+  "Star fruit",
+  "Strawberry",
+  "Tomato",
+  "Zucchini",
 ];
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -67,7 +66,7 @@ let randomWord = wordBank[randomIndex];
 let cpuWord = new Word(randomWord);
 
 let asterisk = "**********************************************************".rainbow;
-let tilde =    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".rainbow;
+let tilde = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".rainbow;
 let fruityIntro = "*.*.*.*.*.*.* ".rainbow;
 
 let requireWord = false;
@@ -85,25 +84,23 @@ function introPrompt() {
     fruityIntro + "WELCOME TO FRUITY WORD GUESS ".white + fruityIntro + "\r\n" + "\r\n" +
     asterisk + "\r\n" + "\r\n"
   );
-  inquirer.prompt([
-    {
-      type: "list",
-      message: "Choose your difficulty!\r\n",
-      choices: ["Easy".green, "Medium".yellow, "Hard".red],
-      name: "difficulty"
+  inquirer.prompt([{
+    type: "list",
+    message: "Choose your difficulty!\r\n",
+    choices: ["Easy".green, "Medium".yellow, "Hard".red],
+    name: "difficulty"
+  }]).then(answers => {
+    let diff = answers.difficulty
+    if (diff === "Easy".green) {
+      guessesLeft = 10;
+      gameLogic();
+    } else if (diff === "Medium".yellow) {
+      guessesLeft = 7;
+      gameLogic();
+    } else {
+      guessesLeft = 3;
+      gameLogic();
     }
-  ]).then(answers => {
-      let diff = answers.difficulty
-      if (diff === "Easy".green) {
-        guessesLeft = 10;
-        gameLogic();
-      } else if (diff === "Medium".yellow) {
-        guessesLeft = 7;
-        gameLogic();
-      } else {
-        guessesLeft = 3;
-        gameLogic();
-      }
   })
 }
 
@@ -114,7 +111,7 @@ function gameLogic() {
   if (requireWord) {
 
     let randomIndex = Math.floor(Math.random() * wordBank.length);
-    let randomWord = wordBank[randomIndex];
+    randomWord = wordBank[randomIndex];
     cpuWord = new Word(randomWord);
     requireWord = false;
   }
@@ -131,60 +128,63 @@ function gameLogic() {
 
   // Begin user prompt if the word is not complete
   if (wordComplete.includes(false)) {
-    inquirer.prompt([
-      {
-        type: "input",
-        message: "Guess a letter from A to Z  ".white + "=>" ,
-        name: "userInput"
-      }
-    ]).then(input => {
+    inquirer.prompt([{
+      type: "input",
+      message: "Guess a letter from A to Z  ".white + "=>",
+      name: "userInput"
+    }]).then(input => {
 
-        // Validate user input
-        if (!alphabet.includes(input.userInput) || input.userInput.length > 1) {
+      // Validate user input
+      if (!alphabet.includes(input.userInput) || input.userInput.length > 1) {
 
-          console.log("\nPlease enter a letter from A to Z... ONE at a time! >.< \n".cyan);
+        console.log("\nPlease enter a letter from A to Z... ONE at a time! >.< \n".cyan);
+        gameLogic();
+      } else {
+        if (wrongLetters.includes(input.userInput) || correctLetters.includes(input.userInput) || input.userInput === "") {
+          console.log("\nYou've already tried that letter or nothing was entered!\n".cyan);
           gameLogic();
-        } else {
-          if (wrongLetters.includes(input.userInput) || correctLetters.includes(input.userInput) || input.userInput === "") {
-            console.log("\nYou've already tried that letter or nothing was entered!\n".cyan);
+        }
+        // User input is valid then do this logic
+        else {
+
+          let checkerArray = [];
+
+          cpuWord.userGuess(input.userInput);
+          cpuWord.objArray.forEach(wordCheck);
+
+          if (checkerArray.join("") === wordComplete.join("")) {
+            console.log("\r\n                * * * ".rainbow + "INCORRECT!".red + " * * *\r\n".rainbow);
+            wrongLetters.push(input.userInput);
+            guessesLeft--;
+          } else {
+            console.log("\r\n                * * * CORRECT! * * *\r\n".yellow);
+            correctLetters.push(input.userInput);
+          }
+
+          // Handle the guesses left
+          if (guessesLeft > 0) {
             gameLogic();
-          } 
-            // User input is valid then do this logic
-            else {
+          } else {
+            losses++;
+            console.log("                The word was:".white, randomWord + "\n");
+            console.log("              * * * * ".red + "GAME OVER!".bgRed.black + " * * * * \n".red);
+            
+            playAgain();
+          }
 
-            let checkerArray = [];
-
-            cpuWord.userGuess(input.userInput);
-            cpuWord.objArray.forEach(wordCheck);
-
-            if (checkerArray.join("") === wordComplete.join("")) {
-              console.log("\r\n                * * * ".rainbow + "INCORRECT!".red + " * * *\r\n".rainbow);
-              wrongLetters.push(input.userInput);
-              guessesLeft --;
-            } else {
-              console.log("\r\n                * * * CORRECT! * * *\r\n".yellow);
-              correctLetters.push(input.userInput);
-            }
-
-            // Handle the guesses left
-            if (guessesLeft > 0) {
-              gameLogic();
-            } else {
-              losses++;
-              console.log("              * * * * ".red + "GAME OVER!".bgRed.black + " * * * * \n".red);
-              playAgain();
-            }
-            function wordCheck(key) {
-              checkerArray.push(key.isGuessed);
-            }
+          function wordCheck(key) {
+            checkerArray.push(key.isGuessed);
           }
         }
+      }
     });
   } else {
     wins++;
+    console.log("               The word was:".white, randomWord + "\n");
     console.log("              * * * * ".yellow + "YOU WIN!".bgYellow.black + " * * * * \n".yellow);
     playAgain();
   }
+
   function completeCheck(key) {
     wordComplete.push(key.isGuessed)
   }
@@ -192,31 +192,29 @@ function gameLogic() {
 
 // Prompt user to play again if they win or lose
 function playAgain() {
-  
+
   console.log("                  Wins: ".yellow + wins + " Losses: ".red + losses + "\r\n");
-  inquirer.prompt([
-    {
+  inquirer.prompt([{
       type: "list",
       message: "Play Again?".rainbow,
       choices: ["Yes", "No".red],
       name: "restart"
-    }
-  ])
-  .then(input => {
-    if (input.restart === "Yes") {
-      requireWord = true;
-      wrongLetters = [];
-      correctLetters = [];
-      guessesLeft = 10;
-      introPrompt();
-    } else {
-      console.log(
-        asterisk + "\r\n" + "\r\n" +
-        "*.*.*.*.*.*.*.*.* ".rainbow + "THANKS FOR PLAYING".white + " *.*.*.*.*.*.*.*.*".rainbow + "\r\n" + "\r\n" +
-        asterisk + "\r\n" + "\r\n"
-      );
-      return;
-    }
-  });
+    }])
+    .then(input => {
+      if (input.restart === "Yes") {
+        requireWord = true;
+        wrongLetters = [];
+        correctLetters = [];
+        guessesLeft = 10;
+        introPrompt();
+      } else {
+        console.log(
+          "\n" + asterisk + "\r\n" + "\r\n" +
+          "*.*.*.*.*.*.*.*.* ".rainbow + " THANKS FOR PLAYING ".white + " *.*.*.*.*.*.*.*.*".rainbow + "\r\n" + "\r\n" +
+          asterisk
+        );
+        return;
+      }
+    });
 }
 introPrompt();
